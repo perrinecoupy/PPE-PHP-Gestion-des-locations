12a0a5b775d048926bd283a4d4b5ec7c4382d320
Arthur <valentin.arthur1000@gmail.com>
2022-03-14 17:18:16 +0100

    Mise à jour gestion locataires / mandataires

---
+++ src/Repository/ResidenceRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use App\Entity\Residence;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+
+/**
+ * @method Residence|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Residence|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Residence[]    findAll()
+ * @method Residence[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LocationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Residence::class);
+    }
+
+}
