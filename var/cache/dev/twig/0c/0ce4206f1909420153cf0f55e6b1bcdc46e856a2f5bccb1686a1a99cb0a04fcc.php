22eaa2e2389f25523c3101ce799af4dacd78defc
Perrine <perrinecoupy@gmail.com>
2022-04-24 18:29:27 +0200

    Mise à jour

---
+++ var/cache/dev/twig/0c/0ce4206f1909420153cf0f55e6b1bcdc46e856a2f5bccb1686a1a99cb0a04fcc.php
@@ -0,0 +1,94 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* includes/_footer.html.twig */
+class __TwigTemplate_e3892239ae8aade9f24ce39aab54957da8c22cbab0398f527e36480ff6bc061f extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "includes/_footer.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "includes/_footer.html.twig"));
+
+        // line 1
+        echo "
+    <div id=\"footerText\">
+        <div class=\"footerLeft\">
+            <ul>
+                <li class=\"infos\">Gestion locative</li>
+                <li class=\"infos\">Contact telephone : 06 06 06 06 06</li>
+                <li class=\"email-contact\">Contact email : contact@gestionlocative.fr</li>
+            </ul>
+        </div>
+        <div class=\"footerRight\">
+            <ul>
+                <li class=\"infos-version\">Version 1.0</li>
+            </ul>
+        </div>
+    </div>";
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "includes/_footer.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("
+    <div id=\"footerText\">
+        <div class=\"footerLeft\">
+            <ul>
+                <li class=\"infos\">Gestion locative</li>
+                <li class=\"infos\">Contact telephone : 06 06 06 06 06</li>
+                <li class=\"email-contact\">Contact email : contact@gestionlocative.fr</li>
+            </ul>
+        </div>
+        <div class=\"footerRight\">
+            <ul>
+                <li class=\"infos-version\">Version 1.0</li>
+            </ul>
+        </div>
+    </div>", "includes/_footer.html.twig", "C:\\Users\\perri\\PPE-PHP-Gestion-des-locations\\templates\\includes\\_footer.html.twig");
+    }
+}
